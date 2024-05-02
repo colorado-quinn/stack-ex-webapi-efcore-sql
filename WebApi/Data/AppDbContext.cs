be19c8ecbfeecf9b168c46af7e067337593ed601
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-02 17:36:25 -0600

    setup EFCore8

---
+++ WebApi/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly string? _connString;
+
+        public AppDbContext(IConfiguration config)
+        {
+            _connString = config.GetConnectionString("DatabaseConnection");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connString);
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
